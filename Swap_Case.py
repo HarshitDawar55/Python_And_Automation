99c8c578dc2da576f2d1a032c6e512ed2626ebd9
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2019-06-17 14:48:01 +0530

    Program to swap cases in a String!

--- Swap_Case.py
+++ Swap_Case.py
@@ -7,7 +7,5 @@ def swap_case(s):
             f.append(i.lower())
         else:
             f.append(i)
-    #f = str(f)
-    #return ''.join(f)
     return ''.join(f)
 print(swap_case("HeLLO I AM harshit dawar"))
